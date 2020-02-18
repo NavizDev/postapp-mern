bfef344f8a9713cc7fda82753cba1f2e283eade9
Juan <juansilupumaza@gmail.com>
2020-02-18 00:58:48 -0500

    [fix] delete node_modules in backend

--- src/backend/index.js
+++ src/backend/index.js
@@ -6,7 +6,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.get("/", function(req, res, next) {
-  return res.send("Hello Nodejs");
+  return res.send("Hello Nodejs, iam Juan. ");
 });
 const port = 8080;
 app.listen(port, () => {
